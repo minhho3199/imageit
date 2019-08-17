152642cdc6544daa4c1b66bcae0d86ffc545a250
youngkikim99 <53206069+youngkikim99@users.noreply.github.com>
2019-08-17 15:31:40 +1000

    Import Logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./Components/DiscussionBoard/HomePage"
+
+import Logo from "./Components/Header/Logo"
+
 function App() {
   return(
     <div className="App">
