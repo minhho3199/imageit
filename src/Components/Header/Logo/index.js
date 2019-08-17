a7ccc76dd53fcdbe50028f1ca168950fe63d65b6
youngkikim99 <53206069+youngkikim99@users.noreply.github.com>
2019-08-17 15:28:59 +1000

    Update index.js

--- src/Components/Header/Logo/index.js
+++ src/Components/Header/Logo/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import logos from './logo.png'
+import './styles.css'
+
+const Logo = () => {
+ return (
+   <div className="logo">
+   <img src={logos} alt="a logo" />
+   </div>
+ );
+};
+
+export default Logo;
 
