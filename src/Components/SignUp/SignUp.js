1afd35854074aa08ed0f33916e50dcc8268c61de
minhho3199 <minhhqhe130268@fpt.edu.vn>
2019-08-28 19:48:03 +1000

    Edit username to name in register form

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -14,10 +14,10 @@ export default class SignUp extends Component {
                         <div className="signup-container">
                               <h2>Sign Up</h2>
                               <form>
-                                    <input type="text" name="username" required placeholder="Enter your username"></input> <br/>
-                                    <input type="text" name="email" required placeholder="Enter your email"></input> <br/>
-                                    <input type="password" name="password" required placeholder="Enter your password"></input> <br/>
-                                    <input type="password" name="retype-password" required placeholder="Reenter your password"></input> <br/>
+                                    <input type="text" required placeholder="Enter your name"></input> <br/>
+                                    <input type="text" required placeholder="Enter your email"></input> <br/>
+                                    <input type="password" required placeholder="Enter your password"></input> <br/>
+                                    <input type="password" required placeholder="Reenter your password"></input> <br/>
                                     <button type="submit" id="signup-button">Sign Up</button>
                               </form>
                         </div>
