5a2eb83851c6303718f1887647332d5b1e41df27
minhho3199 <cryaosis132@gmail.com>
2019-09-26 19:06:13 +1000

    Add loading spinner when fetching all posts

--- Frontend/src/Components/AllPosts/AllPosts.js
+++ Frontend/src/Components/AllPosts/AllPosts.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import "./AllPosts.css";
 import SinglePost from '../SinglePost/SinglePost';
+import Spinner from 'react-bootstrap/Spinner';
+
+// const PostComponent = lazy(() => import('../SinglePost/SinglePost'));
 
 class Discussion extends Component {
       constructor(props) {
             super(props);
             this.state = {
                   posts: [],
+                  loading: true
             }
       }
 
@@ -16,6 +20,7 @@ class Discussion extends Component {
                   .then(res => res.json())
                   .then(data => {
                         this.setState({
+                              loading: false,
                               posts: data,
                         })
 
@@ -25,18 +30,28 @@ class Discussion extends Component {
       render() {
             return (
                   <div id="container">
-                        {this.state.posts.map(post => (
-                              <div className="discussion-container" key={post._id}>
-                                    {/*Passes the post state to SinglePost component as props */}
-                                    <SinglePost id={post._id}
-                                          title={post.title}
-                                          author={post.author.name}
-                                          authorID={post.author._id}
-                                          image={post.image.data}
-                                          reactions={post.reactions} 
-                                          comments={post.comment}/>
-                              </div>
-                        ))}
+                        {this.state.loading ?
+                              <div className="discussion-container loading-container">
+                                    <Spinner animation="border" role="status">
+                                          <span className="sr-only">Loading...</span>
+                                    </Spinner>
+                              </div> :
+                              <div>
+                                    {this.state.posts.map(post => (
+                                          <div className="discussion-container" key={post._id}>
+                                                {/*Passes the post state to SinglePost component as props */}
+                                                <SinglePost id={post._id}
+                                                      title={post.title}
+                                                      author={post.author.name}
+                                                      authorID={post.author._id}
+                                                      image={post.image.data}
+                                                      reactions={post.reactions}
+                                                      comments={post.comment} />
+
+                                          </div>
+                                    ))}
+                              </div>}
+
                   </div>
 
             );
